868e231f5dac78a710d845e11b5e76b517d09b3c
Steven Sheets <smsheets@iastate.edu>
2021-02-27 19:10:17 -0600

    Moved things to complete

--- Backend/tests/gs-accessing-data-mongodb-master/complete/src/main/java/com/example/accessingdatamongodb/Customer.java
+++ Backend/tests/gs-accessing-data-mongodb-master/complete/src/main/java/com/example/accessingdatamongodb/Customer.java
@@ -1,8 +1,9 @@
 package com.example.accessingdatamongodb;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-
+@Document(collection="customers")
 public class Customer {
 
 	@Id
