32ff5819a3477376100b644974034eae6c4ab3e6
Steven Sheets <smsheets@iastate.edu>
2021-03-09 19:27:17 -0600

    Package structure... structured.

---
+++ Backend/src/main/java/app/entities/customers/CustomerRepository.java
@@ -0,0 +1,16 @@
+package app.entities.customers;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+
+    public List<Customer> findByLastName(String lastName);
+
+}
