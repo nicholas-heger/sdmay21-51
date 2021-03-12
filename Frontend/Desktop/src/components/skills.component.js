f0d616e49575c1ab37fc695ff4365efe42e2a3c9
hegerni <hegerni@iastate.edu>
2021-03-12 11:09:19 -0600

    skills list created, GET requests work on testing website

--- Frontend/Desktop/src/components/skills.component.js
+++ Frontend/Desktop/src/components/skills.component.js
@@ -4,10 +4,13 @@ import axios from 'axios';
 
 export default class Skills extends Component {
   state = {
-    // totalReactPackages: null,
+    totalReactPackages: null,
     persons: [],
-    id1: null,
-    test: null
+    name1: null,
+    test: null,
+    personNames: [],
+    id: 1,
+    skills: [],
   }
 
   componentDidMount() {
@@ -16,18 +19,64 @@ export default class Skills extends Component {
     // axios.get('https://jsonplaceholder.typicode.com/users')
     // .then(response => this.setState({ test: response.data }));
 
-    axios.get('http://headers.jsontest.com/')
-        .then(res => {
-        const persons = res.data;
-        this.setState({ persons });
-        this.setState({test: persons.Host});
-    })
+    // axios.get('http://headers.jsontest.com/')
+    //     .then(res => {
+    //     const persons = res.data;
+    //     this.setState({ persons });
+    //     this.setState({test: persons.Host});
+    // })
 
-    //     axios.get(`https://jsonplaceholder.typicode.com/users`)
-    //       .then(res => {
-    //         const persons = res.data;
-    //         this.setState({ persons });
-    //       })
+    axios.get(`https://api.mocki.io/v1/98d5b2a9`)
+          .then(res => {
+            const name1 = res.data[0].name;
+            console.log("RES DATA");
+            console.log(res.data[0].name);
+            this.setState({name1: name1});
+
+            const persons = res.data;
+            console.log("PERSONS");
+            console.log(persons);
+            this.setState({ persons: persons });
+
+            const length = res.data.length;
+            var skills = [];
+            for (var i = 0; i < length; i++) {
+              console.log(res.data[i].id);
+              if(res.data[i].id === this.state.id) {
+                console.log("ID FOUND");
+                const skillLength = res.data[i].skills.length;
+                for (var j = 0; j < skillLength; j++) {
+                  skills[j] = res.data[i].skills[j];
+                }
+              }
+            }
+            this.setState({ skills: skills });
+            console.log(skills);
+
+
+            // console.log(persons.filter((person) => person.id === 1));
+          })
+
+        // axios.get(`https://jsonplaceholder.typicode.com/users`)
+        //   .then(res => {
+        //     const name1 = res.data[0].name;
+        //     console.log("RES DATA");
+        //     console.log(res.data[0].name);
+        //     this.setState({name1: name1});
+
+        //     const persons = res.data;
+        //     console.log("PERSONS");
+        //     console.log(persons);
+        //     this.setState({ persons: persons });
+
+        //     const length = res.data.length;
+        //     var personNames = [];
+        //     for (var i = 0; i < length; i++){
+        //       personNames[i] = res.data[i].name;
+        //     }
+        //     this.setState({personNames: personNames});
+        //     // console.log(persons.filter((person) => person.id === 1));
+        //   })
 
     // axios.get(`https://jsonplaceholder.typicode.com/users`)
     //   .then(res => {
@@ -60,13 +109,42 @@ export default class Skills extends Component {
         //     </div>
         // </div>
 
-        <ul>
-            {this.state.test}
-        </ul>
+        // <ul>
+        //     {this.state.name1}
+        // </ul>
 
-    //   <ul>
-    //     { this.state.persons.map(person => <li>{person.name}</li>)}
-    //   </ul>
+      // <ul>
+      //   { this.state.persons.map(person => <li>{person.name}</li>)}
+      // </ul>
+      <div>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+          Add Skill
+        </button>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                Some Text
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <ul className="list-group">
+          { this.state.skills.map(skill => <li className="list-group-item">{skill}</li>)}
+        </ul>
+      </div>
     )
   }
 }
